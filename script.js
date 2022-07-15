d86f3af06a92e1118fc28866455dfddd78e3e08a
alex <106116143+maslov-cool@users.noreply.github.com>
2022-07-15 23:18:00 +0300

    add

---
+++ script.js
@@ -0,0 +1,42 @@
+const note=document.querySelector('.note')
+const placeholders=document.querySelectorAll('.placeholder')
+
+note.addEventListener('dragstart',dragstart)
+
+note.addEventListener('dragend',dragend)
+
+for (const placeholder of placeholders) {
+   placeholder.addEventListener('dragover',dragover)
+   placeholder.addEventListener('dragenter',dragenter)
+   placeholder.addEventListener('dragleave',dragleave)
+   placeholder.addEventListener('drop',drop)
+}
+
+function dragstart(event) {
+    event.target.classList.add('hold')
+    setTimeout(() => {
+        event.target.classList.add('hide')
+    }, 0);
+    
+}
+function dragend() {
+    event.target.classList.remove('hold','hide')
+}
+
+function dragover(event) {
+    event.preventDefault()
+}
+function dragenter(event) {
+    event.target.classList.add('hovered')
+}
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+}
+function drop(event) {
+    event.target.append(note)
+    event.target.classList.remove('hovered')
+}
+
+
+
+
